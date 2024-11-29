63f504c863a4318ef11dded9614498b3f65f2671
Elisson Deveque <89363543+EDeveque@users.noreply.github.com>
2024-11-29 08:23:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarMais() {
+    const moreInfo = document.getElementById("moreInfo");
+    if (moreInfo.style.display === "none") {
+        moreInfo.style.display = "block";
+    } else {
+        moreInfo.style.display = "none";
+    }
+}
